24bc940bffdae06ae74af581fbd8c50ae4751fdc
Andrey Skobenkov <a.skobenkov@corp.mail.ru>
2023-11-30 17:45:02 +0300

    build pkg

---
+++ cdb/cdb.go
@@ -0,0 +1,134 @@
+package cdb
+
+import (
+	"encoding/json"
+	"math"
+	"regexp"
+	"strings"
+
+	"github.com/colinmarc/cdb"
+	"gopkg.in/yaml.v2"
+)
+
+var floatRE = regexp.MustCompile(`^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][-+]?\d+)?$`) // RFC8259
+
+type WriteItem struct {
+	json  bool
+	Path  string
+	Value string
+	Tp    string
+}
+
+type yamlValue struct {
+	value interface{}
+	str   string
+}
+
+func (v *yamlValue) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	// handle map[interface{}]interface{} separately to prevent unwanted recursion
+	{
+		var value map[string]*yamlValue
+		err := unmarshal(&value)
+		if err == nil {
+			v.value = value
+			return nil
+		}
+	}
+
+	// handle []interface{} separately to prevent unwanted recursion
+	{
+		var value []*yamlValue
+		err := unmarshal(&value)
+		if err == nil {
+			v.value = value
+			return nil
+		}
+	}
+
+	var value interface{}
+	err := unmarshal(&value)
+	if err != nil {
+		return err
+	}
+
+	switch typedVal := value.(type) {
+	case bool: // strict YAML 1.2
+		var str string
+		err := unmarshal(&str)
+		if err != nil {
+			return err
+		}
+		switch str {
+		case "true", "false":
+			v.value = value
+		default:
+			v.value = str
+		}
+		return nil
+	case float64:
+		if math.IsNaN(typedVal) || math.IsInf(typedVal, 0) {
+			v.value = nil
+		} else {
+			v.value = value
+			unmarshal(&v.str)
+			if !floatRE.MatchString(v.str) {
+				v.str = ""
+			}
+		}
+		return nil
+	default:
+		v.value = value
+		return nil
+	}
+}
+
+func (v *yamlValue) MarshalJSON() ([]byte, error) {
+	if v.str != "" {
+		return []byte(v.str), nil
+	}
+	return json.Marshal(v.value)
+}
+
+func YAMLToJSON(y []byte) ([]byte, error) {
+	var data *yamlValue
+	err := yaml.Unmarshal(y, &data)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(data)
+}
+
+func Write(filepath string, params []WriteItem) error {
+	w, err := cdb.Create(filepath)
+	if err != nil {
+		return err
+	}
+
+	for _, param := range params {
+		switch param.Tp {
+		case "application/x-yaml":
+			res, err := YAMLToJSON([]byte(param.Value))
+			if err != nil {
+				return err
+			}
+			param.Value = string(res)
+			param.json = true
+		}
+
+		p := strings.Split(param.Path, "/")
+		param.Path = strings.Join(p, ".")
+		var t string
+		if param.json {
+			t = "j"
+		} else {
+			t = "s"
+		}
+		err = w.Put([]byte(param.Path), []byte(t+param.Value))
+		if err != nil {
+			w.Close()
+			return err
+		}
+	}
+
+	return w.Close()
+}
---
+++ yml/parser/parser.go
@@ -0,0 +1,217 @@
+package parser
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"reflect"
+	"sort"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type OnlineConfItem struct {
+	Key   string
+	Value string
+	Type  string
+}
+
+func GetParentNodeKeys(config map[string]OnlineConfItem) []string {
+	nodes := map[string]bool{}
+	for k := range config {
+
+		list := strings.Split(k, "/")
+
+		nodeKey := ""
+		for _, k := range list[:len(list)-1] {
+			if nodeKey == "" {
+
+				nodeKey = k
+			} else {
+
+				nodeKey = nodeKey + "/" + k
+			}
+			if ok := nodes[nodeKey]; !ok {
+				nodes[nodeKey] = true
+			}
+		}
+	}
+	nodeKeys := []string{}
+	for k := range nodes {
+		nodeKeys = append(nodeKeys, k)
+	}
+
+	sort.Strings(nodeKeys)
+	return nodeKeys
+}
+
+func GetNodeKeysForDelete(config map[string]OnlineConfItem) []string {
+	nodes := map[string]bool{}
+	for k := range config {
+
+		list := strings.Split(k, "/")
+
+		nodeKey := ""
+		for _, k := range list {
+			if nodeKey == "" {
+
+				nodeKey = k
+			} else {
+
+				nodeKey = nodeKey + "/" + k
+			}
+			if ok := nodes[nodeKey]; !ok {
+				nodes[nodeKey] = true
+			}
+		}
+	}
+	nodeKeys := []string{}
+	for k := range nodes {
+		nodeKeys = append(nodeKeys, k)
+	}
+
+	sort.Sort(sort.Reverse(sort.StringSlice(nodeKeys)))
+	return nodeKeys
+}
+
+func WalkByYML(obj reflect.Value, prefix string) map[string]OnlineConfItem {
+	o := make(map[string]OnlineConfItem)
+	switch obj.Kind() {
+	case reflect.Ptr:
+		res := WalkByYML(obj.Elem(), "")
+		o = mergeMaps(o, res)
+	case reflect.Interface:
+		res := WalkByYML(obj.Elem(), prefix)
+		o = mergeMaps(o, res)
+	case reflect.Map:
+		for _, key := range obj.MapKeys() {
+			p := key.Elem().String()
+			if prefix != "" {
+				//				keyValue := ""
+				//				switch key.Elem() {
+				//					case reflect.Int
+				//				}
+				p = fmt.Sprintf("%s/%v", prefix, key.Elem())
+				//				log.Printf("================> %+v", key.Elem())
+				//				log.Printf("================> %+v", key.Elem())
+			}
+			res := WalkByYML(obj.MapIndex(key), p)
+			//			o[prefix] = res
+			o = mergeMaps(o, res)
+		}
+	case reflect.Slice:
+		////		o[prefix] = obj.Interface()
+		//list := []interface{}{}
+		list := []string{}
+		for i := 0; i < obj.Len(); i++ {
+			sliceObj := obj.Index(i)
+			toMarshalIf := sliceObj.Interface()
+			switch v := toMarshalIf.(type) {
+			case string, float64, int, bool:
+				list = append(list, fmt.Sprintf("- %v", v))
+			default:
+				toMarshalIf = []interface{}{v}
+				yamlOption, err := yaml.Marshal(toMarshalIf)
+				if err != nil {
+					log.Fatal(fmt.Errorf("can't marshal %+v to yaml... %+v", sliceObj.Interface(), err))
+				}
+				list = append(list, fmt.Sprintf("%v", string(yamlOption)))
+			}
+		}
+		if len(list) > 0 {
+			o[prefix] = OnlineConfItem{
+				Key:   prefix,
+				Value: strings.Join(list, "\n"),
+				Type:  "application/x-yaml",
+			}
+		}
+	case reflect.String:
+		o[prefix] = OnlineConfItem{
+			Key:   prefix,
+			Value: fmt.Sprintf("%v", obj.Interface()),
+			Type:  "text/plain",
+		}
+	case reflect.Float64:
+		o[prefix] = OnlineConfItem{
+			Key:   prefix,
+			Value: fmt.Sprintf("%v", obj.Interface()),
+			Type:  "text/plain",
+		}
+	case reflect.Int:
+		o[prefix] = OnlineConfItem{
+			Key:   prefix,
+			Value: fmt.Sprintf("%v", obj.Interface()),
+			Type:  "text/plain",
+		}
+	case reflect.Bool:
+		o[prefix] = OnlineConfItem{
+			Key:   prefix,
+			Value: fmt.Sprintf("%v", obj.Interface()),
+			Type:  "text/plain",
+		}
+	}
+	return o
+}
+
+func GetYMLConfig(filepath string) (interface{}, error) {
+	var data interface{}
+
+	readFile, err := os.Open(filepath)
+	if err != nil {
+		return data, err
+	}
+	defer readFile.Close()
+	reader := bufio.NewReader(readFile)
+	yamlProcessed := ""
+	lineNumber := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if len(line) == 0 && err != nil {
+			if err == io.EOF {
+				break
+			}
+			return data, err
+		}
+		line = strings.ReplaceAll(line, ": \"", ": \"")
+
+		listRow := strings.Split(line, ":")
+		if len(listRow) == 2 {
+			listRow[1] = strings.TrimSpace(listRow[1])
+			if listRow[1] != "" {
+				if _, err := strconv.Atoi(listRow[1]); err != nil {
+					listRow[1] = strings.ReplaceAll(listRow[1], "'", "")
+					listRow[1] = strings.ReplaceAll(listRow[1], "\"", "")
+					listRow[1] = fmt.Sprintf("'%s'", listRow[1])
+				}
+			}
+			line = strings.Join(listRow, ": ") + "\n"
+		}
+		lineNumber++
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return data, err
+		}
+		yamlProcessed += line
+	}
+
+	err = yaml.Unmarshal([]byte(yamlProcessed), &data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, err
+}
+
+func mergeMaps(a, b map[string]OnlineConfItem) map[string]OnlineConfItem {
+	for k, v := range b {
+		a[k] = v
+	}
+	return a
+}
